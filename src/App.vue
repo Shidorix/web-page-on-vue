6f86dfddfcd5acf40c8c26b5c1d698bebffdd327
Shidorix <danila.mazepa@gmail.com>
2023-07-07 02:14:15 +0300

    Добавил основу

---
+++ src/App.vue
@@ -0,0 +1,9 @@
+<template>
+  <router-view v-slot="{ Component }">
+    <keep-alive exclude="Document">
+      <component :is="Component" />
+    </keep-alive>
+  </router-view>
+</template>
+
+<script setup lang="ts"></script>
---
+++ src/layouts/MainLayout.vue
@@ -0,0 +1,1 @@
+<template>Привет</template>
