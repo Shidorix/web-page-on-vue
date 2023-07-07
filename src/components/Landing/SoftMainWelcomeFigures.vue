99f86ca448d317c7319a898654dffe1583f1dea8
Shidorix <danila.mazepa@gmail.com>
2023-07-07 23:37:06 +0300

    Прогресс с фигурами

---
+++ src/components/Landing/SoftMainWelcomeFigures.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="pocket"></div>
+</template>
+
+<style scoped lang="scss">
+.pocket {
+  position: relative;
+  width: 100%;
+  height: 500px;
+}
+
+.pocket::before,
+.pocket::after {
+  content: '';
+  position: absolute;
+  bottom: 350px;
+  width: calc((1vw + 1vh) * 33);
+  height: 500px;
+}
+
+.pocket::before {
+  left: 50%;
+  background-image: url('../../assets/image/Subtract.png');
+  transform: scale(-1, 1);
+}
+
+.pocket::after {
+  right: 50%;
+  background-image: url('../../assets/image/Subtract.png');
+  //   transform: scale(-1, 1)
+}
+</style>
--- src/pages/IndexPage.vue
+++ src/pages/IndexPage.vue
@@ -2,10 +2,12 @@
   <q-page style="overflow-x: hidden">
     <main>
       <soft-welcome />
+      <soft-main-welcome-figures />
     </main>
   </q-page>
 </template>
 
 <script setup lang="ts">
 import SoftWelcome from '../components/Landing/SoftWelcome.vue';
+import SoftMainWelcomeFigures from '../components/Landing/SoftMainWelcomeFigures.vue';
 </script>
