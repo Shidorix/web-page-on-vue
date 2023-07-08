41f1fcdd8c75eb43384b16f0b215adfef72f7624
Shidorix <danila.mazepa@gmail.com>
2023-07-08 17:14:13 +0300

    Еще фигуру подправил

--- src/components/Landing/SoftMainWelcomeFigures.vue
+++ src/components/Landing/SoftMainWelcomeFigures.vue
@@ -5,28 +5,30 @@
 <style scoped lang="scss">
 .pocket {
   position: relative;
-  width: 100%;
-  height: 22em;
-}
-
-.pocket::before,
-.pocket::after {
-  content: '';
-  position: absolute;
-  bottom: 300px;
   width: calc((1vw + 1vh) * 70);
-  height: 100%;
+  height: 210px;
+  background-image: url('../../assets/image/Subtract4.svg');
+  bottom: 208px;
 }
 
-.pocket::before {
-  left: 49%;
-  background-image: url('../../assets/image/Subtract2.png');
-  transform: scale(-1, 1);
-}
+// .pocket::before,
+// .pocket::after {
+//   content: '';
+//   position: absolute;
+//   bottom: 300px;
+//   width: calc((1vw + 1vh) * 70);
+//   height: 100%;
+// }
 
-.pocket::after {
-  right: 49%;
-  background-image: url('../../assets/image/Subtract2.png');
-  //   transform: scale(-1, 1)
-}
+// .pocket::before {
+//   left: 49%;
+//   background-image: url('../../assets/image/Subtract2.png');
+//   transform: scale(-1, 1);
+// }
+
+// .pocket::after {
+//   right: 49%;
+//   background-image: url('../../assets/image/Subtract2.png');
+//   //   transform: scale(-1, 1)
+// }
 </style>
