02854090016360f505b3fec9ccf9b1c528152d43
Shidorix <danila.mazepa@gmail.com>
2023-07-19 23:07:35 +0300

    Вроде установил GSAP

---
+++ src/sdk/animations/anim.js
@@ -0,0 +1,12 @@
+gsap.registerPlugin(ScrollTrigger, ScrollSmoother)
+
+if (ScrollTrigger.isTouch !== 1) {
+
+	ScrollSmoother.create({
+		wrapper: '.wrapper',
+		content: '.content',
+		smooth: 2,
+		effects: true
+	})
+
+}
